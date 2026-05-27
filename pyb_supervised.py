21b8aa326f158168cb888761ba6dff463b04304a
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2026-05-27 14:40:58 +0000

    train the model

--- pyb_supervised.py
+++ pyb_supervised.py
@@ -1,7 +1,7 @@
 # https://aiml4os.github.io/funathon-project2/1-ttc.html
 
 # %% Imports
-# import mlflow
+import mlflow
 import polars as pl
 from dotenv import load_dotenv
 from sklearn.model_selection import train_test_split
@@ -9,6 +9,8 @@ from sklearn.preprocessing import LabelEncoder
 from torchTextClassifiers.value_encoder import ValueEncoder
 from torchTextClassifiers.tokenizers import WordPieceTokenizer
 from torchTextClassifiers import ModelConfig, TrainingConfig, torchTextClassifiers
+import s3fs
+import random
 
 load_dotenv(override=True)
 # os.getenv('MLFLOW_TRACKING_USERNAME')
@@ -88,4 +90,64 @@ ttc = torchTextClassifiers(
     model_config=model_config,
     value_encoder=value_encoder,
 )
-# %%
+
+# %%  Prepare training
+training_config = TrainingConfig(
+    lr=5e-4,  # a standard starting learning rate for Adam-based optimizers
+    batch_size=128,  # examples processed before wt update, larger are faster but more memory
+    num_epochs=1,  # one pass over the data, kept short for this demo
+    patience_early_stopping=5  # to avoid overfitting
+)
+
+# %% Train on a small subsample
+mlflow.set_experiment("funathon-2026-project2_PYB")
+mlflow.pytorch.autolog()
+
+with mlflow.start_run() as run:
+    # This should take approximately 1-2 min
+    ttc.train(
+        X_train,
+        y_train,
+        training_config=training_config,
+        X_val=X_val,
+        y_val=y_val,
+        verbose=True,
+    )
+
+    mlflow.log_artifacts(
+        training_config.save_path,   # local folder produced by ttc.train()
+        artifact_path="model_artifacts",
+    )
+
+# # %%
+# #| label: load-from-run
+# #| code-overflow: scroll
+# #| output: true
+# local_dir = mlflow.artifacts.download_artifacts(
+#     f"runs:/{run.info.run_id}/model_artifacts"
+# )
+
+# # Rebuild the torchTextClassifiers object from the downloaded files
+# ttc_loaded = torchTextClassifiers.load(local_dir)
+
+# print(ttc_loaded)
+
+# %% Load a better model
+
+fs = s3fs.S3FileSystem(
+    anon=True,  # public bucket
+    endpoint_url="https://minio.lab.sspcloud.fr",
+)
+
+local_dir = "./mlflow-artifacts/"
+fs.get(
+    "projet-funathon/diffusion/mlflow-artifacts/",
+    local_dir,
+    recursive=True,
+)
+# Rebuild the torchTextClassifiers object from the downloaded files
+ttc = torchTextClassifiers.load(local_dir)
+
+ttc.pytorch_model.eval()
+
+# %% Generate top-5 predictions with confidence scores
