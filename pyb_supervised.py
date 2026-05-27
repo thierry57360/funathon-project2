bb355a644a2902345c86f55f5a0da501d9347d83
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2026-05-27 13:06:10 +0000

    tokenize

--- pyb_supervised.py
+++ pyb_supervised.py
@@ -1,10 +1,13 @@
+# https://aiml4os.github.io/funathon-project2/1-ttc.html
+
 # %% Imports
-import numpy as np
-import mlflow
+# import mlflow
 import polars as pl
 from dotenv import load_dotenv
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from torchTextClassifiers.value_encoder import ValueEncoder
+from torchTextClassifiers.tokenizers import WordPieceTokenizer
 
 load_dotenv(override=True)
 # os.getenv('MLFLOW_TRACKING_USERNAME')
@@ -46,5 +49,31 @@ else:
 
 encoder = LabelEncoder()
 encoder.fit(train_df['code'].to_numpy())
+
+# to get back code instead of integers
+value_encoder = ValueEncoder(label_encoder=encoder)
+
+# %% Train the tokenizer
+
+tokenizer = WordPieceTokenizer(vocab_size=5000, output_dim=10)
+tokenizer.train(X_train)
+
+# (A Tensor is a multi-dimensional matrix containing elements of a single data type)
+print("Output tensor size:", tokenizer.tokenize(X_train[0]).input_ids.shape)
+print("Vocabulary size:", tokenizer.vocab_size)
+
+# %%
+# Look at an example of tokenization
+print("Raw text : ", X_train[554])
+print(
+    "Tokens id:",
+    tokenizer.tokenize(X_train[554]).input_ids.squeeze(0)
+)
+print(
+    "Tokens:",
+    tokenizer.tokenizer.convert_ids_to_tokens(
+        tokenizer.tokenize(X_train[554]).input_ids.squeeze(0)
+    )
+)
 
 # %%
