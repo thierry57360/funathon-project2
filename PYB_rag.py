981950df862bc674119666b6380211f953070d22
Pierre-Yves Berrard <pierre-yves.berrard@insee.fr>
2026-05-28 09:35:40 +0000

    imports nace

--- PYB_rag.py
+++ PYB_rag.py
@@ -2,6 +2,9 @@
 
 from dotenv import load_dotenv
 import os
+from openai import OpenAI
+from qdrant_client import QdrantClient
+import duckdb
 
 # %% Load environment variables from .env file
 # QDRANT_URL=https://YOURNAMESPACE-qdrant.user.lab.sspcloud.fr/
@@ -20,7 +23,6 @@ except KeyError:
     raise ValueError("QDRANT_URL is not set — check your .env file")
 
 # %% Conncet to LLM API
-from openai import OpenAI  # noqa: E402
 
 client_llmlab = OpenAI(
     base_url=os.environ["LLMLAB_URL"],
@@ -33,7 +35,6 @@ for model in models.data:
     print(f"ID: {model.id}")
 
 # %% Connet to qdrant
-from qdrant_client import QdrantClient  # noqa: E402
 
 client_qdrant = QdrantClient(
     url=os.environ["QDRANT_URL"],
@@ -42,9 +43,21 @@ client_qdrant = QdrantClient(
     check_compatibility=False
 )
 
-# %%
 collections = client_qdrant.get_collections()
 for collection in collections.collections:
     print(collection.name)
 
+# %% Import NACE data as a list of dictionaries
+con = duckdb.connect(database=":memory:")
+
+con.execute("INSTALL httpfs;")
+con.execute("LOAD httpfs;")
+
+path_nace = 'https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/NACE_Rev2.1_Structure_Explanatory_Notes_EN.tsv'  # noqa: E501
+query_definition = f"SELECT * FROM read_csv('{path_nace}')"
+table = con.execute(query_definition).to_arrow_table()
+nace = table.to_pylist()
+
+# %%
+nace[22]
 # %%
